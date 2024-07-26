529958f50932b5fe42ad0b82dad2073cbe2fec0c
IvanRavic88 <ravic.ivan88@gmail.com>
2024-07-26 13:04:29 +0200

    Import svg changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,47 +1,47 @@
-"use client";
-import Link from "next/link";
-import Image from "next/image";
-import { motion } from "framer-motion";
-import { ContactForm } from "@/components/ContactForm";
-import EmailIcon from "@/public/email_1.svg";
-import GitHubIcon from "@/public/github_1.svg";
-import LinkedInIcon from "@/public/linkedin_1.svg";
-import PhoneIcon from "@/public/phone-icon.svg";
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { ContactForm } from '@/components/ContactForm';
+import EmailIcon from '@/public/svg/email_1.svg';
+import GitHubIcon from '@/public/svg/github_1.svg';
+import LinkedInIcon from '@/public/svg/linkedin_1.svg';
+import PhoneIcon from '@/public/svg/phone-icon.svg';
 
 export default function ContactPage() {
   return (
     <section
       style={{ backgroundImage: `url("/hero-img_1.png" )` }}
-      className="bg-cover bg-right md:bg-center bg-no-repeat min-h-screen flex flex-col p-4 md:p-6 lg:p-12 xl:p-28 "
+      className="flex min-h-screen flex-col bg-cover bg-right bg-no-repeat p-4 md:bg-center md:p-6 lg:p-12 xl:p-28"
     >
-      <div className="text-left text-white grid gap-8 lg:gap-16 md:grid-cols-1 lg:grid-cols-2 w-full bg-black/40">
+      <div className="grid w-full gap-8 bg-black/40 text-left text-white md:grid-cols-1 lg:grid-cols-2 lg:gap-16">
         <motion.div
           initial={{ x: -100, opacity: 0 }} // Animating from left
           animate={{ x: 0, opacity: 1 }} // Moving to the center
-          transition={{ ease: "easeInOut", duration: 2 }}
+          transition={{ ease: 'easeInOut', duration: 2 }}
           className="flex flex-col gap-6"
         >
-          <div className="p-4 sm:p-6 md:p-8 lg:p-10 w-full">
-            <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-custom-red my-6 md:my-8 lg:my-10">
+          <div className="w-full p-4 sm:p-6 md:p-8 lg:p-10">
+            <h2 className="my-6 text-2xl font-bold text-custom-red md:my-8 md:text-3xl lg:my-10 lg:text-4xl">
               Let&apos;s talk
             </h2>
 
-            <div className="flex flex-col gap-4 w-full">
-              <div className="flex flex-row gap-2 md:gap-3 items-center">
+            <div className="flex w-full flex-col gap-4">
+              <div className="flex flex-row items-center gap-2 md:gap-3">
                 <Image
                   src={EmailIcon}
                   alt="White email icon."
                   width={24}
                   height={24}
                 />
-                <Link href={"mailto:ravic.ivan88@gmail.com"}>
-                  <p className="text-sm md:text-lg hover:underline hover:text-custom-yellow">
+                <Link href={'mailto:ravic.ivan88@gmail.com'}>
+                  <p className="text-sm hover:text-custom-yellow hover:underline md:text-lg">
                     ravic.ivan88@gmail.com
                   </p>
                 </Link>
               </div>
 
-              <div className="flex flex-row gap-2 md:gap-3 items-center">
+              <div className="flex flex-row items-center gap-2 md:gap-3">
                 <Image
                   src={LinkedInIcon}
                   alt="White icon for LinkedIn."
@@ -49,41 +49,41 @@ export default function ContactPage() {
                   height={24}
                 />
                 <Link
-                  href={"https://www.linkedin.com/in/ivan-ravić-b3aa36143"}
+                  href={'https://www.linkedin.com/in/ivan-ravić-b3aa36143'}
                   target="_blank"
                 >
-                  <p className="text-sm md:text-lg hover:underline hover:text-custom-yellow">
+                  <p className="text-sm hover:text-custom-yellow hover:underline md:text-lg">
                     LinkedIn Profile
                   </p>
                 </Link>
               </div>
 
-              <div className="flex flex-row gap-2 md:gap-3 items-center">
+              <div className="flex flex-row items-center gap-2 md:gap-3">
                 <Image
                   src={GitHubIcon}
                   alt="White github icon."
                   width={24}
                   height={24}
-                />{" "}
+                />{' '}
                 <Link
-                  href={"https://github.com/IvanRavic88?tab=repositories"}
+                  href={'https://github.com/IvanRavic88?tab=repositories'}
                   target="_blank"
                 >
-                  <p className="text-sm md:text-lg hover:underline hover:text-custom-yellow">
+                  <p className="text-sm hover:text-custom-yellow hover:underline md:text-lg">
                     GitHub Profile
                   </p>
                 </Link>
               </div>
 
-              <div className="flex flex-row gap-2 md:gap-3 items-center">
+              <div className="flex flex-row items-center gap-2 md:gap-3">
                 <Image
                   src={PhoneIcon}
                   alt="White phone icon."
                   width={24}
                   height={24}
                 />
-                <Link href={"tel:+381655254013"}>
-                  <p className="text-sm md:text-lg hover:underline hover:text-custom-yellow">
+                <Link href={'tel:+381655254013'}>
+                  <p className="text-sm hover:text-custom-yellow hover:underline md:text-lg">
                     +381655254013
                   </p>
                 </Link>
